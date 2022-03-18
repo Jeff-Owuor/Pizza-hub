f80c3813773c0333b733e16dde8060065d702118
Jeff <Xavierjeff451@gmail.com>
2022-03-18 17:27:11 +0300

    commit

--- app.js
+++ app.js
@@ -23,6 +23,10 @@ $(document).ready(function(){
         $(".imageContainer2").show();
         $(this).hide();
     })
+    $("#buttonEl").click(function(){
+        (".tabular").show();
+        generateOutput();
+    })
 })
 
 
@@ -31,10 +35,14 @@ $(document).ready(function(){
 
 
 
-let buttonEl=document.getElementById("buttonEl");
-let crustEl=document.getElementById("crusts");
-let sizeEl=document.getElementById("size");
-let toppingsEl=document.getElementById("toppings");
+let crustEl=document.getElementById("crusts").value;
+let sizeEl=document.getElementById("size").value;
+let toppingsEl=document.getElementById("toppings").value;
+let tableEl=document.getElementById("spaning");
+let tableEl2=document.getElementById("spanning")
+let tableEl3=document.getElementById("spanner");
+let tableEl4=document.getElementById("span")
+let tableEl5=document.getElementById("another")
 const sizes={
     "large":"ksh800",
     "medium":"ksh700",
@@ -49,4 +57,25 @@ const crusts={
     "stuffed":"ksh200",
     "gluten-free":"ksh350",
     "crispy":"ksh400"
+}
+function generateOutput(){
+    if(crustEl=="stuffed"&&sizeEl=="small"&&toppingsEl=="mushroom"){
+          tableEl.innerText=1;
+          tableEl2.innerText=`${sizeEl}-${sizes.small}`;
+          tableEl3.innerText=`${toppingsEl}-${toppings.mushroom}`
+          tableEl4.innerText=`${crustEl}-${crusts.stuffed}`
+          tableEl5.innerText="ksh1050"
+    }else if(crustEl=="stuffed"&&sizeEl=="medium"&&toppingsEl=="mushroom"){
+        tableEl.innerText=1;
+        tableEl2.innerText=`${sizeEl}-${sizes.medium}`;
+        tableEl3.innerText=`${toppingsEl}-${toppings.mushroom}`
+        tableEl4.innerText=`${crustEl}-${crusts.stuffed}`
+        tableEl5.innerText="ksh1150"
+    }else if(crustEl=="stuffed"&&sizeEl=="large"&&toppingsEl=="mushroom"){
+        tableEl.innerText=1;
+        tableEl2.innerText=`${sizeEl}-${sizes.medium}`;
+        tableEl3.innerText=`${toppingsEl}-${toppings.mushroom}`
+        tableEl4.innerText=`${crustEl}-${crusts.stuffed}`
+        tableEl5.innerText="ksh1250" 
+    }
 }
