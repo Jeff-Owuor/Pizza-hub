4b986e55f5bb41c31a68d8435abb61d198e8bba3
Jeff <Xavierjeff451@gmail.com>
2022-03-18 17:04:17 +0300

    commit

--- app.js
+++ app.js
@@ -32,6 +32,21 @@ $(document).ready(function(){
 
 
 let buttonEl=document.getElementById("buttonEl");
-let crustEl=document.getElementById("crusts")
+let crustEl=document.getElementById("crusts");
 let sizeEl=document.getElementById("size");
 let toppingsEl=document.getElementById("toppings");
+const sizes={
+    "large":"ksh800",
+    "medium":"ksh700",
+    "small":"ksh 600"
+}
+const toppings={
+   "mushroom":"ksh250",
+    "bacon":"ksh200",
+    "ham":"ksh300"
+}
+const crusts={
+    "stuffed":"ksh200",
+    "gluten-free":"ksh350",
+    "crispy":"ksh400"
+}
